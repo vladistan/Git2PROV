b308592fd15bd80c4a9a3f2959322f1134cdcf21
Vlad <vlad@v-lad.org>
2016-05-06 22:46:27 -0400

    Fixup CI build script to work on Docker 1.7.1 (linux only) and post fix coverage file

--- ci/build.sh
+++ ci/build.sh
@@ -3,6 +3,12 @@ NODEMOD_VOLUME=git2.prov.nodemod.${GO_PIPELINE_NAME}
 
 VOL_COMMANDS="-v $NPM_VOLUME:/app/.npm -v $NODEMOD_VOLUME:/app/node_modules"
 
+DOCKER_VERSION=$(docker --version | sed 's/[^0-9]//g')
+if [ x${DOCKER_VERSION} == x17178629171  ]; then
+   echo "Old Docker detected"
+   VOL_COMMANDS=''
+fi
+
 docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u 0:0 \
        $VOL_COMMANDS \
        -i -e HOME=/app vladistan/node \
@@ -34,5 +40,7 @@ docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u $UID:$UID \
        $VOL_COMMANDS \
        -i -e HOME=/app vladistan/node \
        npm run-script cov-jenkins
+
+sed  -i.bak  's@^SF:/app/@SF:@' coverage/lcov.info
 
 /usr/local/sonar-runner/bin/sonar-runner
