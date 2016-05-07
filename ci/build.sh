a7c59ad1207c5d111d4bd02086c151b278d41cd5
Vlad <vlad@v-lad.org>
2016-05-06 21:32:00 -0400

    Use docker volumes for npm and node_modules

--- ci/build.sh
+++ ci/build.sh
@@ -1,18 +1,37 @@
+NPM_VOLUME=git2.prov.npm.${GO_PIPELINE_NAME}
+NODEMOD_VOLUME=git2.prov.nodemod.${GO_PIPELINE_NAME}
+
+VOL_COMMANDS="-v $NPM_VOLUME:/app/.npm -v $NODEMOD_VOLUME:/app/node_modules"
+
+docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u 0:0 \
+       $VOL_COMMANDS \
+       -i -e HOME=/app vladistan/node \
+       chown -R $UID /app/.npm
+
+docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u 0:0 \
+       $VOL_COMMANDS \
+       -i -e HOME=/app vladistan/node \
+       chown -R $UID /app/node_modules
+
 docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u $UID:$UID \
+       $VOL_COMMANDS \
        -i -e HOME=/app vladistan/node \
        npm install
 
 docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u $UID:$UID \
+       $VOL_COMMANDS \
        -i -e HOME=/app vladistan/node \
        npm run-script test
 
 docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u $UID:$UID \
+       $VOL_COMMANDS \
        -i -e HOME=/app vladistan/node \
        npm run-script test-cov
 
 
 
 docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u $UID:$UID \
+       $VOL_COMMANDS \
        -i -e HOME=/app vladistan/node \
        npm run-script cov-jenkins
 
