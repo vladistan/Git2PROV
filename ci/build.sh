2f7ec72a22b7d6452c5c6830a88e87ce6a6d9d3a
Vlad <vlad@v-lad.org>
2016-09-30 19:33:41 -0400

    Make ci script not fail when npm dirs exist

--- ci/build.sh
+++ ci/build.sh
@@ -10,9 +10,11 @@ DOCKER_VERSION=$(docker --version | sed 's/[^0-9]//g')
 if [ x${DOCKER_VERSION} == x17178629171  ]; then
    echo "Old Docker detected"
    VOL_COMMANDS=''
-   mkdir .npm
-   mkdir mode_modules
+   mkdir -p .npm
+   mkdir -p node_modules
 fi
+
+echo "My UID : ${UID}"
 
 docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u 0:0 \
        $VOL_COMMANDS \
